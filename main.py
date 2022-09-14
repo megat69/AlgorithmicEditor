fdf632e06effb3db89b6e6928251acc9c3b86842
megat69 <assassincraft218@gmail.com>
2022-09-14 17:08:44 +0200

    Added command compiling !

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import curses
 import sys
+import pyperclip
 
 
 class App:
@@ -9,6 +10,8 @@ class App:
 		self.rows, self.cols = 0, 0
 		self.lines = 1
 		self.current_index = 0
+		self.commands = (("q", self.quit, "Quit"), ("c", self.compile, "Compile"))
+		self.instructions_list = []
 
 
 	def main(self, stdscr):
@@ -30,7 +33,7 @@ class App:
 			if key == ":":
 				self.stdscr.addstr(self.rows - 1, 0, ":")
 				key = self.stdscr.getkey()
-				for key_name, function in (("q", self.quit), ("c", self.compile)):
+				for key_name, function, name in self.commands:
 					if key == key_name:
 						self.stdscr.addstr(self.rows - 1, 1, key_name)
 						self.stdscr.refresh()
@@ -41,6 +44,8 @@ class App:
 			else:
 				# Screen clearing
 				self.stdscr.clear()
+
+				# TODO : Cursor placement
 
 				# If the key is NOT a backspace character, we add the new character to the text
 				if key == "\b" or key.startswith("KEY_"):
@@ -54,14 +59,15 @@ class App:
 						pass
 					elif key == "KEY_LEFT":
 						self.current_index -= 1
-					elif key == "KEY_DOWN":
+					elif key == "KEY_RIGHT":
 						self.current_index += 1
-					# Clamping the index
-					self.current_index = max(min(self.current_index, len(self.current_text)), 0)
 				# If the key IS a backspace character, we remove the last character from the text
 				else:
 					self.current_text = self.current_text[:self.current_index] + key + self.current_text[self.current_index:]
 					self.current_index += 1
+
+				# Clamping the index
+				self.current_index = max(min(self.current_index, len(self.current_text)), 0)
 
 			# Displays the current text
 			for i, line in enumerate(self.current_text.split("\n")):
@@ -87,7 +93,14 @@ class App:
 		"""
 		# Applies the bar at the bottom of the screen
 		self.stdscr.addstr(self.rows - 3, 0, "▓" * self.cols)
-		self.stdscr.addstr(self.rows - 2, 0, ":q - Quit", curses.A_REVERSE | curses.A_BOLD)
+		cols = 0
+		for key_name, function, name in self.commands:
+			generated_str = f":{key_name} - {name}"
+			self.stdscr.addstr(self.rows - 2, cols, generated_str, curses.A_REVERSE | curses.A_BOLD)
+			cols += len(generated_str)
+			self.stdscr.addstr(self.rows - 2, cols, " ")
+			cols += 1
+		self.stdscr.refresh()
 
 		# Gets the amount of lines in the text
 		self.calculate_line_numbers()
@@ -109,7 +122,70 @@ class App:
 		"""
 		Compiles the inputted text into algorithmic code.
 		"""
-		pass
+		self.instructions_list = self.current_text.split("\n")
+		instructions_stack = []
+		for i, line in enumerate(self.instructions_list):
+			line = line.split(" ")
+			instruction_name = line[0]
+			instruction_params = line[1:]
+
+			if instruction_name in ("int", "float", "string", "bool", "char"):
+				var_type = ""
+				if instruction_name == "int":
+					var_type = "Entier"
+				elif instruction_name == "float":
+					var_type = "Réel"
+				elif instruction_name == "string":
+					var_type = "Chaîne de caractères"
+				elif instruction_name == "bool":
+					var_type = "Booléen"
+				elif instruction_name == "char":
+					var_type = "Caractère"
+				self.instructions_list[i] = ", ".join(instruction_params) + " : " + var_type + \
+				                            ("s" if len(instruction_params) != 1 and instruction_name != "string" else "")
+
+			elif instruction_name == "for":
+				instructions_stack.append("for")
+				self.instructions_list[i] = f"Pour {instruction_params[0]} allant de {instruction_params[1]} à " \
+				                            f"{instruction_params[2]} avec un pas de " \
+				                            f"{1 if len(instruction_params) < 4 else instruction_params[3]}"
+
+			elif instruction_name == "end":
+				names = {'for': "Pour", 'if': "Si", "while": "Tant Que"}
+				last_elem = instructions_stack.pop()
+				self.instructions_list[i] = f"Fin {names[last_elem]}"
+
+			elif instruction_name == "while":
+				instructions_stack.append("while")
+				self.instructions_list[i] = f"Tant Que {' '.join(instruction_params)}"
+
+			elif instruction_name == "if":
+				instructions_stack.append("if")
+				self.instructions_list[i] = f"Si {' '.join(instruction_params)}"
+
+			elif instruction_name == "print":
+				self.instructions_list[i] = f"Afficher({' '.join(instruction_params)})"
+
+			elif instruction_name == "input":
+				self.instructions_list[i] = f"Saisir({' '.join(instruction_params)})"
+
+			elif instruction_params[0] == "=":
+				self.instructions_list[i] = f"{instruction_name} ← {' '.join(instruction_params[1:])}"
+
+			elif instruction_params[0].endswith("="):
+				self.instructions_list[i] = f"{instruction_name} ← {instruction_name} {instruction_params[0][:-1]} {' '.join(instruction_params[1:])}"
+
+			self.instructions_list[i] = "\t" * (len(instructions_stack) - (1 if instruction_name in ("if", "while", "for") else 0)) + self.instructions_list[i]
+
+		final_compiled_code = "Début\n" + "".join("\t" + instruction + "\n" for instruction in self.instructions_list) + "Fin"
+		pyperclip.copy(final_compiled_code)
+		self.stdscr.clear()
+		self.stdscr.addstr(final_compiled_code)
+		self.stdscr.refresh()
+		self.stdscr.getch()
+		self.stdscr.clear()
+		self.apply_stylings()
+		self.stdscr.refresh()
 
 
 if __name__ == "__main__":
