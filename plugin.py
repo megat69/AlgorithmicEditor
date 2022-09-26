bd258f356faf093a131caf5e0fef186047cb0320
megat69 <assassincraft218@gmail.com>
2022-09-26 20:51:45 +0200

    Documented plugins

--- plugin.py
+++ plugin.py
@@ -4,7 +4,19 @@ class Plugin:
 	def __init__(self, cls):
 		self.cls = cls
 
+	def init(self):
+		"""
+		Called when loading the plugin.
+		"""
+		pass
+
 	def add_command(self, character:str, function:Callable, description:str):
+		"""
+		Adds a command to the app.
+		:param character: The character triggering the command.
+		:param function: The function called on command trigger.
+		:param description: A very short description of the command shown to the user (less than 20 characters).
+		"""
 		if len(character) != 1:
 			raise Exception(f"Command character length must be one character, not {len(character)} ('{character}').")
 		else:
