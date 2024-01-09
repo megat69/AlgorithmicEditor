63c6e140bc99b318d1268b1a9becf0e4c09d0bb5
megat69 <assassincraft218@gmail.com>
2024-01-09 23:32:47 +0100

    Added CTRL+O to open a file

--- main.py
+++ main.py
@@ -389,6 +389,8 @@ class App:
 				self.add_char_to_text('!')
 			elif key == "PADENTER":
 				self.add_char_to_text('\n')
+			elif len(key) == 1 and ord(key) == 15:  # CTRL+O to open a file
+				self.commands['o'][0]()
 			"""elif key == "KEY_HOME":
 				self.min_display_char -= 1
 				if self.min_display_char < 0:
